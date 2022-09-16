d14b03da91c05b1feec4d783129fbcd7c36a5b19
IvetteMcDermott <ivettedemcdermott@gmail.com>
2022-09-16 11:23:10 +0000

    Modifications for mix of stories pick, and addition of arts

--- lines_and_stories.py
+++ lines_and_stories.py
@@ -1,6 +1,19 @@
-WELC_LINE = 'To spooky stories time...hope you are ready to be scared.\n'
 
-story_picked = ['The Disembodied Voice','Do not turn out the lights','The Scratches in the Closet']
+WELC_LINE = '\n This is spooky story time...hope you are ready to be scared.\n'
+
+story_picked = ['draw in your room.','to do your homework in your room.','take a nap.']
+
+LETS_BEGIN = "Good, so let's begin.\n"
+
+introduction = ['It was a hot spring night in Florida.', ' had just gone home from school and settled down to do her homework in her old wooden house.\n','It had been in the family for over 100 years. And it creaked and groaned as the winds blew.\n']
+
+first_part = ['To',' , they were the familiar sounds of a house full of love, one never used to be silent.',' fondly remembered her grandmother cooking bashing pots and pans in the kitchen.','Granny would yell all around whe dinner was ready.']
+
+secon_part = []
+
+third_part = []
+
+
 
 
 
--- run.py
+++ run.py
@@ -20,12 +20,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("pp3-python")
 
-trial = SHEET.worksheet("Sheet1")
-trial_values = trial.get_all_values()
-
 
 def slowType(data):
-    """ Slow down the print time, taken from tutorial, credit in README """
+    """ Slow down the print time for one line, taken from tutorial, credit in README """
 
     for character in data:
         sys.stdout.write(character)
@@ -35,7 +32,7 @@ def slowType(data):
 
 
 def slowTypeF(data):
-    """ Slow down the print time, taken from tutorial, credit in README """
+    """ Slow down the print time but continue, for long texts, taken from tutorial, credit in README """
 
     for character in data:
         sys.stdout.write(character)
@@ -44,7 +41,7 @@ def slowTypeF(data):
     return ""
 
 def slowTypeDif(data):
-    """ Slow down the print time, taken from tutorial, credit in README """
+    """ Slow down the print time with 2 sets of time, taken from tutorial, credit in README """
 
     for character in data:
         sys.stdout.write(character)
@@ -57,112 +54,90 @@ def slowTypeDif(data):
 
 
 def welcome():
+    """ Display the welcome message """
     slowTypeF(f"\033[1;31;40m {ascii_img.welcome}")
     slowType(f"\033[1;31;40m {lines_and_stories.WELC_LINE} \n")
+    slowType('Only rule here is that you need to answer my questions where you see =>, so I can continue.\n')
 
+
+name = ''
 
 def introduction():
-    slowType('Now tell me your name ...')
+    """ Collect the user's name and validate it to no be empty """
+    slowType('\n'
+        'Now tell me your name ...\n')
     name = input('=>')
     if name != '':
         time.sleep(3)
-        slowType(f'{name}, that name, it reminds me of a story...\n')
+        slowType('\n'
+            f'{name}, that name, it reminds me of a story...\n')
         age()
     else:
-        slowType('You are not following the rules...')
+        slowType('You are not following the rules...\n')
         introduction()
+    return name
+
+def age():
+    slowType('\n'f'but first, tell me your age {name}...\n')
+    age = input('=>')
+    if int(age) >= 10:
+        slowType('\n'
+            f'{age}?...you are old enough to hear it but are you brave enough?\n')
+        slowType('\n'
+            "Do you want to continue? 'y' for yes or 'n' for no \n")
+        answer = input('=>')
+        if answer == 'n':
+            slowType('\n'
+                'So you chicken out? I did not expect that from you.\n')
+            print(ascii_img.chicken)
+        else:
+            slowType('\n'
+                "Seems I meet a brave one, let's start...\n")
+            pick_story()
+    elif age == '':
+        slowType('You are not following the rules...\n''Try again...')
+        age()
+    else:
+        slowType('\n'
+        'Oh you are too young yet, sorry I may offer you in a next time a lullaby...\n')
+        slowType(ascii_img.baby)
+        slowType('Bye for now...')
 
 
 def pick_story():
-    slowType('Tell me which one would you like me to tell you? pick a number\n')
-    slowType('1. The Disembodied Voice. \n  2.Do not turn out the lights.\n  3.The Scratches in the Closet\n')
+    intro = lines_and_stories.introduction[0]
+    intro1 = lines_and_stories.introduction[1]
+    intro2 = lines_and_stories.introduction[2]
+    print(ascii_img.house)
+    slowType(f"{intro} {name} {intro1} {intro2}")
+    slowType('\n''Now tell me what would you like do?\n')
+    slowType('\n'
+        ' 1.Draw in your room.\n 2.Do your homework.\n 3.Take a nap.\n')
+    slowType('\n'
+        'Now pick one (1 or 2 or 3)\n')
     stor_pick = input('=>')
     picked = ''
     if int(stor_pick) == 1:
         picked = lines_and_stories.story_picked[0]
-        slowType(f'You had picked "{picked}"')
+        slowType('\n'f'You had picked "{picked}" {lines_and_stories.LETS_BEGIN}')
     elif int(stor_pick) == 2:
         picked = lines_and_stories.story_picked[1]
-        slowType(f'You had picked "{picked}"')
+        slowType('\n'f'You had picked "{picked}" {lines_and_stories.LETS_BEGIN}')
     elif int(stor_pick) == 3:
         picked = lines_and_stories.story_picked[2]
-        slowType(f'You had picked "{picked}"')
+        slowType('\n'f'You had picked "{picked}" {lines_and_stories.LETS_BEGIN}')
     else:
-        slowType('You had not pick a valid option, try again...')
+        slowType('\n''You had not pick a valid option, try again...\n')
         pick_story()
+    story_part_one()
 
 
 def story_part_one():
     print('ta-da')
 
 
-def age():
-    slowType('but first, tell me your age...\n')
-    age = input('=>')
-    if int(age) >= 10:
-        slowType(f'{age}?...you are old enough to hear it but are you brave enough?\n')
-        slowType("Do you want to continue? 'y' for yes or 'n' for no \n")
-        answer = input('=>')
-        if answer == 'n':
-            slowType('So you chicken out? I did not expect that from you.\n')
-            print(ascii_img.chicken)
-        else:
-            slowType("Seems I meet a brave one, let's start...\n")
-            pick_story()
-    else:
-        slowType('Oh you are too young yet, sorry I may offer you in a next time a lullaby...\n')
-        slowType(ascii_img.baby)
-        slowType('Bye for now...')
-
-
 welcome()
 introduction()
 
-
-"""slowprint("It's time for me to tell you some spooky stories... but first,...\n")
-name = slowprint('Tell me your name ...\n')"""
-
-
-"""
-def get_name(name):
-    name = get_name('Tell me your name ...\n')
-    validation_input(name)    
-    return name
-
-def get_age(prompt):
-    return int(input(prompt))
-
-def validation_input(name):
-     idea took from love-sandwiches
-        to manage data no empty
-    
-    while name != '':
-        print(f'{name}... That name reminds me of a story... mmm...')
-        get_age('but first, you need to tell me your age...')
-        return name
-    else:
-        print('You need to tell me your name...\n')
-        get_name(name)
-
-
-validation_input(name)
-
-# time.sleep(4)
-
-    
-
-# time.sleep(4)
-# print(f'{age}?...you are old enough to hear it but are you brave enough?\n')
-# brave = input('"y" for yes and "n" for no...\n')
-
-# if brave == 'y':
-#     time.sleep(3)
-#     print('Which would you prefer...?\n')
-#     # pick_story()
-# elif brave == 'n':
-#     time.sleep(2)
-#     print("So you chicken out? I didn't expect that from you.\n")
-#     print(ascii_img.chicken)
-#     print('Maybe one day I should offer a lullaby?\n')
-#     print(ascii_img.baby)
-"""
+# users_record = SHEET.worksheet("Sheet1")
+# users_record.append_row(name)
